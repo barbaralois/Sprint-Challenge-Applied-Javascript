be6c066d800a5ba177c4a105f9a7eb9134a53ba4
Barbara Moore <barbaraloismoore@gmail.com>
2020-05-08 12:03:25 -0400

    completed MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,50 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+const articleEntry = document.querySelector('.cards-container');
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((response) => {
+    let topics = response.data.articles;
+    let indiTopics = Object.keys(topics);
+    for (let i = 0; i < indiTopics.length; i++) {
+      topics[indiTopics[i]].forEach((article) => {
+        let newArticle = createCard(article);
+        articleEntry.appendChild(newArticle);
+      });
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log('Article GET request completed');
+  });
+
+function createCard(articleData) {
+  let { headline, authorPhoto, authorName } = articleData;
+  const card = document.createElement('div');
+  const headlineDiv = document.createElement('div');
+  const authorContainer = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const image = document.createElement('img');
+  const author = document.createElement('span');
+
+  card.appendChild(headlineDiv);
+  card.appendChild(authorContainer);
+  authorContainer.appendChild(imgContainer);
+  imgContainer.appendChild(image);
+  authorContainer.appendChild(author);
+
+  card.classList.add('card');
+  headlineDiv.classList.add('headline');
+  authorContainer.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  headlineDiv.textContent = headline;
+  image.src = authorPhoto;
+  author.textContent = `By ${authorName}`;
+
+  return card;
+}
