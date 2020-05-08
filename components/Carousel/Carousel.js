38b822e775ee8beaa33a72b453f97918e2de98a5
Barbara Moore <barbaraloismoore@gmail.com>
2020-05-08 12:49:22 -0400

    completed image carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,66 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselEntry = document.querySelector('.carousel-container');
+
+function createCarousel() {
+  let carouselDiv = document.createElement('div');
+  let leftButton = document.createElement('div');
+  let imgOne = document.createElement('img');
+  let imgTwo = document.createElement('img');
+  let imgThree = document.createElement('img');
+  let imgFour = document.createElement('img');
+  let rightButton = document.createElement('div');
+
+  carouselDiv.appendChild(leftButton);
+  carouselDiv.appendChild(imgOne);
+  carouselDiv.appendChild(imgTwo);
+  carouselDiv.appendChild(imgThree);
+  carouselDiv.appendChild(imgFour);
+  carouselDiv.appendChild(rightButton);
+
+  carouselDiv.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  imgOne.src = './assets/carousel/mountains.jpeg';
+  imgTwo.src = './assets/carousel/computer.jpeg';
+  imgThree.src = './assets/carousel/trees.jpeg';
+  imgFour.src = './assets/carousel/turntable.jpeg';
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = '<';
+  rightButton.textContent = '>';
+
+  let picContainer = [imgOne, imgTwo, imgThree, imgFour];
+  let currNum = 0;
+  let activePic = picContainer[currNum];
+  activePic.style.display = 'block';
+
+  leftButton.addEventListener('click', (event) => {
+    activePic.style.display = 'none';
+    let newNum = currNum - 1;
+    if (newNum < 0) {
+      newNum = 3;
+      currNum = 3;
+    }
+    currNum = newNum;
+    activePic = picContainer[newNum];
+    activePic.style.display = 'block';
+  });
+
+  rightButton.addEventListener('click', (event) => {
+    activePic.style.display = 'none';
+    let newNum = currNum + 1;
+    if (newNum > 3) {
+      newNum = 0;
+      currNum = 0;
+    }
+    currNum = newNum;
+    activePic = picContainer[newNum];
+    activePic.style.display = 'block';
+  });
+
+  return carouselDiv;
+}
+
+carouselEntry.appendChild(createCarousel());
